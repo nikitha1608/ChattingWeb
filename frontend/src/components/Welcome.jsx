e175887d6059d48bee5d1236cc87bcbac5c78ec3
Nikitha Tripuraneni <94820058+nikitha1608@users.noreply.github.com>
2023-06-24 12:13:18 +0530

    reactjs frontend part

---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      ).username
+    );
+  }, []);
+  return (
+    <Container>
+      <img src={Robot} alt="" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <br />
+      <h3>  Talk To Your Friend To Make Your Bond Stronger. </h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #fff;
+  }
+`;
